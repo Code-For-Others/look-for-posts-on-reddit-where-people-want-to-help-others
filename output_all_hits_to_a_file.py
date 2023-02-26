ca9d19602505ab7a84861148fed442798a7f2764
Max Pietsch <maxwell.pietsch@gmail.com>
2023-02-26 17:41:10 +0000

    automatically committing storage.json file

--- output_all_hits_to_a_file.py
+++ output_all_hits_to_a_file.py
@@ -2,5 +2,10 @@ import json
 
 with open('storage.json', 'r') as storage_file:
     history = json.load(storage_file)
+    hits = []
     for permalink, metadata in history.items():
-        if (
+        if metadata['usefulness'] == 'HIT':
+            hits.append(permalink)
+    with open('hits.txt', 'w') as hits_file:
+        hits_file.write('\n'.join(hits))
+
